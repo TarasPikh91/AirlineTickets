6311f77949f252e04de72368bb477a4eaf417faf
nightlyds <danil.saf.ole@gmail.com>
2017-11-12 15:36:12 +0200

    123

--- src/main/webapp/WEB-INF/views/skyflow.jsp
+++ src/main/webapp/WEB-INF/views/skyflow.jsp
@@ -117,7 +117,7 @@
    </div>
   </div>
   <div class="content2">
-    <h1 id="AboutSkyFlow">About SkyFlow</h1>
+    <h1 id="AboutSkyFlow">About SkyFloW</h1>
     <p id="p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <img src="../css/images/specialoffer.png" alt="specialoffer" id="specialOffer">
     <p id="p2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.</p>
---
+++ target/AirlineTickets-1.0-SNAPSHOT/WEB-INF/js/script.js
@@ -0,0 +1,32 @@
+// package main;
+//
+// import java.io.File;
+// import java.io.FileNotFoundException;
+// import java.util.Scanner;
+//
+// public class main {
+//     public static void main(String args[]) throws FileNotFoundException {
+//         Scanner scan = new Scanner(System.in);
+//         System.out.println("enter count layers");
+//         int layers = scan.nextInt();
+//         System.out.println("enter count of radius");
+//         double radius = scan.nextDouble();
+//         if (layers >= 1 && layers <= 100 || radius >= 1 && radius <= 100) {
+//             double t = 2.0;
+//             double s = Math.pow(radius, t);
+//             double squar = s / layers;
+//             double radiusSmall = Math.sqrt(squar);
+//             System.out.println(radiusSmall);
+//         }
+//     }
+// }
+$(document).scroll(function() {
+  if ($(document).width() > 785) {
+    if ($(document).scrollTop() > $('#header').height() 100){
+      $('#header').height();
+    }
+    else {
+      $('#header').height() + 150;
+    }
+  }
+});
